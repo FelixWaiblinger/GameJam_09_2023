21918d7eb8126d4fff6cc86a00ae28d40f65f878
DESKTOP-DS19ABT\Maurice <maurice-hoeppner@web.de>
2023-09-06 16:58:34 +0200

    some cleanup

---
+++ Assets/UI/Components/UpgradeTooltip.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.UIElements;
+
+public class UpgradeTooltip : VisualElement {
+
+    public UpgradeTooltip(string name, string description) {
+        this.AddToClassList("upgradeTooltip");
+
+        Label _name = this.Create<Label>("title");
+        _name.text = name;
+        this.Add(_name);
+
+        Label _description = this.Create<Label>("text");
+        _description.text = description;
+        this.Add(_description);
+
+    }
+
+}
+
