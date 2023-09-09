8d246f5d13676a3b1568c4c4edb8bd6f71d335c3
DESKTOP-DS19ABT\Maurice <maurice-hoeppner@web.de>
2023-09-09 12:48:23 +0200

    add levelSystem

--- Assets/Scripts/Player/StatusController.cs
+++ Assets/Scripts/Player/StatusController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class StatusController : MonoBehaviour, IDamagable
@@ -8,11 +9,28 @@ public class StatusController : MonoBehaviour, IDamagable
     [SerializeField] private VoidEventChannel _deathEvent;
     [SerializeField] private float _maxHealth;
     private float _currentHealth;
-    private float _maxExperience, _currentExperience;
+
+    private LevelSystem levelSystem;
+
+
+    void Awake() {
+        levelSystem = new LevelSystem(10, 5);
+    }
+
+
+    private void Update() {
+        AddExp(1);
+    }
+
 
     void Start()
     {
         _currentHealth = _maxHealth;
+    }
+
+    public void AddExp(float exp) {
+        levelSystem.AddExp(exp);
+        _experienceEvent.RaiseFloatEvent(levelSystem.GetExpRequiredNormalized());
     }
 
     public void TakeDamage(float amount)
@@ -23,5 +41,40 @@ public class StatusController : MonoBehaviour, IDamagable
         _hitEvent.RaiseVoidEvent();
 
         if (_currentHealth < 0) _deathEvent.RaiseVoidEvent();
+    }
+}
+
+
+public class LevelSystem {
+
+    public int Level { get; private set; } = 1;
+    private float _experience = 0;
+    private float _expToNextLevel;
+    private float _expIncreasePerLevel;
+
+    public LevelSystem(float expToNextLevel, float expIncreasePerLevel) {
+        _expToNextLevel = expToNextLevel;
+        _expIncreasePerLevel = expIncreasePerLevel;
+    }
+
+    public void AddExp(float exp) {
+        _experience += exp;
+        ProcessExp();
+    }
+
+    private void ProcessExp() {
+        while(_experience >= _expToNextLevel) {
+            LevelUp();
+        }
+    }
+
+    private void LevelUp() {
+        _experience -= _expToNextLevel;
+        Level++;
+        _expToNextLevel += _expIncreasePerLevel;
+    }
+
+    public float GetExpRequiredNormalized() {
+        return _experience / _expToNextLevel;
     }
 }
